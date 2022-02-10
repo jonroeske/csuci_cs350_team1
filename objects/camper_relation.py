ff6f98f77407341df016be461f2dbe012a7f0e4f
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-10 13:02:29 -0800

    added camper relation object

---
+++ objects/camper_relation.py
@@ -0,0 +1,6 @@
+class CamperRelation:
+    def __int__(self, requestingcamper, relatedcamper, relationfloat):
+        self.requestingcamper = requestingcamper
+        self.relatedcamper = relatedcamper
+        self.relationfloat = relationfloat
+    
