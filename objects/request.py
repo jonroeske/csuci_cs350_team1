eab04b62fbc6d77bba41dd405931c63bfd1642b2
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-10 13:00:11 -0800

    added request object

---
+++ objects/request.py
@@ -0,0 +1,8 @@
+class Request:
+    def __int__(self, bunkhousefor, bunkhouseagainst,
+                tribefor, tribeagainst):
+        self.bunkhousefor = bunkhousefor
+        self.bunkhouseagainst = bunkhouseagainst
+        self.tribefor = tribefor
+        self.tribeagainst = tribeagainst
+    
