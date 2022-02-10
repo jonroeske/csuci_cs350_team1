e7a0a6924745de55c161180be966d9861fb1c9e9
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-10 12:53:58 -0800

    added camper object

---
+++ objects/camper.py
@@ -0,0 +1,19 @@
+class Camper:
+    def __int__(self, fullname, age, gender, address, appstatus, balance,
+                session, tribe, bunkhouse, acceptstatus, arrivalreqcert,
+                checkedin, assignmentrequest, datesentnotice):
+        self.fullname = fullname
+        self.age = age
+        self.gender = gender
+        self.address = address
+        self.appstatus = appstatus
+        self.balance = balance
+        self.session = session
+        self.tribe = tribe
+        self.bunkhouse = bunkhouse
+        self.acceptstatus = acceptstatus
+        self.arrivalreqcert = arrivalreqcert
+        self.checkedin = checkedin
+        self.assignmentrequest = assignmentrequest
+        self.datesentnotice = datesentnotice
+        
