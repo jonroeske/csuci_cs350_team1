a63526f5ca83885cfca23e0ec7fb035ccf769734
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-11 14:28:56 -0800

    building cli command parser

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 # CLI System
+import sys
+
+
+def main(argv):
+    opts, args = str(argv)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('Help')
+            sys.exit(2)
+        elif opt in ():
+            print('')
+            sys.exit(2)
+
+
+main(sys.argv[1:])
