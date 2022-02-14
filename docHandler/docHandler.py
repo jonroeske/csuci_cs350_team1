121e4086ef353eef5717db8bd8368c27e7faeddf
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-13 16:30:24 -0800

    added funcs docHandler.py

--- docHandler/docHandler.py
+++ docHandler/docHandler.py
@@ -1,30 +1,30 @@
 # Clerk logic for handling camp documents
 import camper
 
-def submitApplication(fullname, age, gender, address):
+def submitApplication(fullname, age, gender, address): # create camper obj using fields
 
 
-def withdrawApplication(fullname):
+def withdrawApplication(fullname): # remove camper obj
 
 
-def reduceBalance(fullname, amount):
+def reduceBalance(fullname, amount): # reduce balance in camper obj by amount
 
 
-def raiseBalance(fullname, amount):
+def raiseBalance(fullname, amount): # raise balance in camper obj by amount
 
 
-def resetBalance(fullname):
+def resetBalance(fullname): # zero balance in camper obj
 
 
-def sentAcceptanceNotice(fullname, date):
+def sentAcceptanceNotice(fullname, date): # sent acceptance notice to camper on date
 
 
-def camperAcceptedNotice(fullname, date):
+def camperAcceptedNotice(fullname, date): # camper accepted notice on date
 
 
-def camperDeclinedNotice(fullname, date):
+def camperDeclinedNotice(fullname, date): # camper declined notice on date
 
 
-def instructionPacketSent(fullname):
+def instructionPacketSent(fullname): # first day instruction packet sent to camper
 
 
