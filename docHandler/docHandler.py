9f6066c074015824eb37e0955489ac6b37fe93d7
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-13 16:28:30 -0800

    added funcs docHandler.py

--- docHandler/docHandler.py
+++ docHandler/docHandler.py
@@ -1,1 +1,30 @@
 # Clerk logic for handling camp documents
+import camper
+
+def submitApplication(fullname, age, gender, address):
+
+
+def withdrawApplication(fullname):
+
+
+def reduceBalance(fullname, amount):
+
+
+def raiseBalance(fullname, amount):
+
+
+def resetBalance(fullname):
+
+
+def sentAcceptanceNotice(fullname, date):
+
+
+def camperAcceptedNotice(fullname, date):
+
+
+def camperDeclinedNotice(fullname, date):
+
+
+def instructionPacketSent(fullname):
+
+
