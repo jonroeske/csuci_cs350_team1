e693502924d45272ce7beead5e546b24b6f2e1ce
Aaron Urrea <aaron.urrea041@myci.csuci.edu>
2022-02-15 15:17:18 -0800

    Added basic functions, might delete later

---
+++ docHandler/payments.py
@@ -0,0 +1,8 @@
+#Handler to be able to accept and deny payments
+# If incremental payments/payment plans, may be a good idea to think ahead
+# Functions include:
+#  camperReduceBalance()
+#  camperCheckBalance()
+#  camperClearBalance()
+
+# AARON URREA
---
+++ postAcceptance/refunds.py
@@ -0,0 +1,4 @@
+#Handler to be able to accept, deny, or challenge refunds
+# Remember time requirements,
+
+# AARON URREA
