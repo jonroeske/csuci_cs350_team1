599bb795d73f24c14a1853943eaa04e9896cffe0
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-19 16:09:56 -0800

    Delete payments.py
    deprecated

--- docHandler/payments.py
+++
@@ -1,8 +0,0 @@
-#Handler to be able to accept and deny payments
-# If incremental payments/payment plans, may be a good idea to think ahead
-# Functions include:
-#  camperReduceBalance()
-#  camperCheckBalance()
-#  camperClearBalance()
-
-# AARON URREA
