1cb65a0ff4a8f9a44b6cd7a160a8ffd582948fd9
Jon Roeske <49179336+jonroeske@users.noreply.github.com>
2022-02-24 10:37:09 -0800

    firstDay.py

--- firstDay/firstDay.py
+++ firstDay/firstDay.py
@@ -2,9 +2,24 @@
 def checkInCert(camper):
     print('')
 
-def assignBunkhouses(campers[], bunkhouses[], requests[]):
-    print('')
 
-def assignTribes(campers[], tribes[], requests[]):
+def assignBunkhouses(campers,bunkhouses):  # 1-3 girl houses, 4-6 boy houses with 12 each
+    housenum = 1
+    for camper in campers:
+        if camper.gender == 'male':
+            housenum += 3
+        if not bunkhouses[housenum].isFull():
+            if not bunkhouses[housenum+1].isFull():
+                if not bunkhouses[housenum+2].isFull():
+                    print('No capacity for camper')
+                else:
+                    bunkhouses[housenum+2].append(camper)
+            else:
+                bunkhouses[housenum+1].append(camper)
+        else:
+            bunkhouses[housenum].append(camper)
+
+
+def assignTribes(campers):
     print('')
 
