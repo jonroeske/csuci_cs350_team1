b74069a9a4bca7619478cfd38d5b20521c66edb4
Jon Roeske <jproeske22@gmail.com>
2022-03-15 17:10:56 -0700

    postAcceptance.py

--- postAcceptance/postAcceptance.py
+++ postAcceptance/postAcceptance.py
@@ -1,25 +1,30 @@
 import datetime
-from datatime import date
-from objects.camper import Camper
+from Handlers.docHandler import raiseBalance
 
 # Clerk logic for camper post acceptance criteria
-#cancellation refund
-#90% within 3 weeks of acceptance and arrival instructions sent
-#45% within 6 weeks of sending
-#0% after this
+# Cancellation refund
+# 90% within 3 weeks of acceptance and arrival instructions sent
+# 45% within 6 weeks of sending
+# 0% after this
 
-def cancellationRefund(Camper):
+
+def cancellationRefund(camper):
     current_time = datetime.datetime.now()
-    today = date(current_time.year, current_time.month, current_time.day)
-    difference = (today - Camper.dateSentNotice).days / 7
+    today = datetime.date(current_time.year, current_time.month, current_time.day)
+    difference = (today - camper.dateSentNotice).days / 7
     if 3 > difference:
         print("Cancelled within 3 weeks - 90% Refund")
-        raiseBalance(Camper.fullName, 900)
+        raiseBalance(camper.fullName, 900)
         return False
     elif 6 > difference:
         print("Cancelled within 6 weeks - 45% Refund")
-        raiseBalance(Camper.fullName, 450)
+        raiseBalance(camper.fullName, 450)
         return False
     else:
         print("Cancellation too late for refund")
         return False
+
+
+def withdrawCamper(camper):
+    print('')
+
