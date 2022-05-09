0fc2be475990a81bf0ac1cc2182e4b31f01c8e49
Aaron Urrea <aaron.urrea041@myci.csuci.edu>
2022-05-09 15:55:05 -0700

    Fixed Library dependencies, cleaned up MUCH of fakers bloat

---
+++ Libraries/faker/__init__.py
@@ -0,0 +1,7 @@
+from Libraries.faker.factory import Factory
+from Libraries.faker.generator import Generator
+from Libraries.faker.proxy import Faker
+
+VERSION = "13.11.0"
+
+__all__ = ("Factory", "Generator", "Faker")
